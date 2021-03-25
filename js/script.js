687b617df8be37f0eb405e7220d1f40fe1c9e6a5
Uldis Grunde-Zeiferts <uldis.grunde@gmail.com>
2021-03-26 00:35:18 +0200

    save

---
+++ js/script.js
@@ -0,0 +1,67 @@
+var x = document.getElementById("demo");
+var xx = document.getElementById("demos");
+
+function getLocation() {
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(showPosition);
+	} else { 
+		x.innerHTML = "Geolocation is not supported by this browser.";
+	}
+}
+
+function showPosition(position) {
+	x.innerHTML = `Latitude:${position.coords.latitude}<br>Longitude: ${position.coords.longitude}`;
+	xx.value=`${position.coords.latitude}\t${position.coords.longitude}`;
+}
+function myFunction() {
+	var copyText = document.getElementById("demos");
+	copyText.select();
+	copyText.setSelectionRange(0, 99999)
+	document.execCommand("copy");
+	document.getElementById("demos").style.visibility = "hidden";
+	alert("Nokopētais teksts: " + copyText.value);
+
+}
+let saveFile = () => {
+const name = document.getElementById('demos');
+let data = 
+		'\r Name: ' + name.value+'\r\n '
+		const textToBLOB = new Blob([data], { type: 'text/plain' });
+	const sFileName = 'formData.txt';	   // The file to save the data.
+
+	let newLink = document.createElement("a");
+	newLink.download = sFileName;
+
+	if (window.webkitURL != null) {
+		newLink.href = window.webkitURL.createObjectURL(textToBLOB);
+	}
+	else {
+		newLink.href = window.URL.createObjectURL(textToBLOB);
+		newLink.style.display = "none";
+		document.body.appendChild(newLink);
+	}
+
+	newLink.click(); 
+}
+
+function copy() {
+	let copyText = document.getElementById("demos");
+	copyText.select();
+	copyText.setSelectionRange(0, 99999)
+	document.execCommand("copy");
+	alert("Nokopētais teksts: " + copyText.value);
+}
+
+function save() {
+	var x = document.getElementById("demo");
+	download("download.txt", x.innerHTML)
+}
+function download(filename, text) {
+	let element = document.createElement('a');
+	element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+	element.setAttribute('download', filename);
+	element.style.display = 'none';
+	document.body.appendChild(element);
+	element.click();
+	document.body.removeChild(element);
+}
